e02f7d12ae2987bc5c8021bc7f97d22cf64c59d6
Nurbek <21njouraboev@gmail.com>
2022-06-07 11:59:05 -0400

    webpack has been set up

--- server/server.js
+++ server/server.js
@@ -8,6 +8,23 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// catch-all router handler for any request to an unknown route
+app.use('*', (req, res) => {
+  return res.status(404).send('ERROR, ROUTE NOT FOUND');
+});
+
+// global error handling
+app.use((err, req, res, next) => {
+  const globalErr = {
+    log: 'UNKNOWN MIDDLEWARE ERROR',
+    status: 500,
+    message: { err: 'ERROR' },
+  };
+  const errorObj = Object.assign({}, globalErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
 });
